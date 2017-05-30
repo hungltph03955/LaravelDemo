a69273d83782c689ad5b493a4f035e59852c216a
hungltph03955 <hungltph03955@fpt.edu.vn>
2017-05-31 00:26:19 +0700

    source code firt

---
+++ app/TheLoai.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TheLoai extends Model
+{
+     protected $table = 'theloai';
+
+    public function loaitin()
+	{
+		return $this->hasMany('App\LoaiTin','idTheLoai','id');
+	}
+
+	public function tintuc()
+	{
+		return $this->hasManyThrought('App\TinTuc','App\LoaiTin','idTheLoai','idLoaiTin','id');
+	}
+
+ 
+
+}
